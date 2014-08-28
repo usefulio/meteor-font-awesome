aac8dc458af334d9fff3e8a31113dff4232f0ac8
Ian Serlin <ian@ianserlin.com>
2014-08-28 01:24:19 -0700

    Revert "0.9.0 update"
    This reverts commit 0fb4c58c2c9ab4e561341a7b5c7f2ddf171f9e86.

--- package.js
+++ package.js
@@ -1,11 +1,7 @@
 Package.describe({
-	name: "natestrauser:font-awesome",
-  summary: "Latest version Font-Awesome loaded via CDN",
-  version: "4.1.1",
-  git: "https://github.com/nate-strauser/meteor-font-awesome.git"
+	summary: "Latest version Font-Awesome loaded via CDN"
 });
 
 Package.on_use(function (api){
-  api.versionsFrom("METEOR@0.9.0");
 	api.add_files('load.js', 'client');
 });
